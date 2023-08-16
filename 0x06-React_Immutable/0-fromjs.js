df0f7c400ed991f22f8feb5e38b25ee67135e645
chigari101 <chigari101@gmail.com>
2023-08-16 09:05:32 -0700

    fixed files

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,14 +3,3 @@ import { fromJS } from 'immutable';
 function getImmutableObject(object) {
   return fromJS(object);
 }
-
-// Example usage
-const inputObject = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-const immutableMap = getImmutableObject(inputObject);
-console.log(immutableMap);
--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,22 +1,5 @@
-import { is } from 'immutable';
+import {is} from 'immutable';
 
-function areMapsEqual(map1, map2) {
+export default function areMapsEqual(map1, map2) {
   return is(map1, map2);
 }
-
-// Example usage
-const map1 = new Map(
-  {
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-  }
-);
-const map2 = new Map(
-  {
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-  }
-);
-
-const result = areMapsEqual(map1, map2);
-console.log(result); // Should output: true
