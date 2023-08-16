6a86643dd8620d81a830b01989c7e49b37bb43b5
chigari101 <chigari101@gmail.com>
2023-08-16 08:46:51 -0700

    fixed files

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,16 @@
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
 function getImmutableObject(object) {
-    return fromJS(object);
+  return fromJS(object);
 }
 
-module.exports = getImmutableObject;
+// Example usage
+const inputObject = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+const immutableMap = getImmutableObject(inputObject);
+console.log(immutableMap);
